381dbf5b11de5955d29d0dd0c7fc64fdfc2ae690
Teolisio10 <57806372+Teolisio10@users.noreply.github.com>
2020-03-20 00:40:30 +0100

    Add files via upload

---
+++ Dati/createRaces_Scores.sql
@@ -0,0 +1,9 @@
+CREATE TABLE [dbo].[Races_Scores](
+	[id] INT NOT NULL,
+	[extDriver] INT NOT NULL,
+	[extPos] INT NOT NULL,
+	[extScore] INT NOT NULL,
+	[extRace] INT NOT NULL,
+	[fastestLap] VARCHAR(16) NOT NULL,
+	CONSTRAINT [PK_Races_Scores] PRIMARY KEY CLUSTERED ([id] ASC)
+ );
---
+++ Dati/createScores.sql
@@ -0,0 +1,6 @@
+CREATE TABLE [dbo].[Scores](
+	[id] [int] NOT NULL,
+	[score] [int] NOT NULL,
+	[details] [varchar](254) NOT NULL,
+ CONSTRAINT [PK_Scores] PRIMARY KEY CLUSTERED ([id] ASC)
+ );
