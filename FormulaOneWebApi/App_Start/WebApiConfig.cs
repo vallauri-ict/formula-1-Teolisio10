f6737f82533175f5f576beaf4bcfe9043c4f340e
Teolisio10 <m.fina.0532@vallauri.edu>
2020-04-02 17:08:42 +0200

    WebApiConfig.cs modifica

--- FormulaOneWebApi/App_Start/WebApiConfig.cs
+++ FormulaOneWebApi/App_Start/WebApiConfig.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Http;
+using System.Net.Http.Headers;
 
 namespace FormulaOneWebApi
 {
@@ -19,6 +20,9 @@ namespace FormulaOneWebApi
                 routeTemplate: "api/{controller}/{cc}",
                 defaults: new { cc = RouteParameter.Optional }
             );
+
+            // PER RICHIEDERE UN JSON SE CI SONO PROBLEMI CON XML
+            // config.Formatters.JsonFormatter.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
         }
     }
 }
